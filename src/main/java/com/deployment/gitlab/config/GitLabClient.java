b7aa77171386da3c8dd2d27f495d7f20de4c5ffd
Clement <clem.brunel@gmail.com>
2025-11-20 17:06:17 +0100

    fix: resolve build problems

--- src/main/java/com/deployment/gitlab/config/GitLabClient.java
+++ src/main/java/com/deployment/gitlab/config/GitLabClient.java
@@ -8,7 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
-import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 
